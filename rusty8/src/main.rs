dc63662266af31fcc4a59ac2e38d5548cd09e0d9
Stefan <7905425+budach@users.noreply.github.com>
2025-09-16 12:41:49 +0200

    WIP rust version

--- rusty8/src/main.rs
+++ rusty8/src/main.rs
@@ -1,3 +1,149 @@
+use std::env;
+use std::fs;
+
+const MEMORY_SIZE: usize = 4096;
+const PROGRAM_START: usize = 0x200;
+const FONTSET_START: usize = 0x50;
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
+
+struct Chip8 {
+    memory: [u8; MEMORY_SIZE], // TODO also test Vec
+    gfx: [u8; SCREEN_WIDTH * SCREEN_HEIGHT],
+    pc: usize,
+    stack: Vec<usize>,
+    v: [u8; 16],
+    i: usize,
+}
+
+impl Chip8 {
+    fn new(filename: &str) -> Self {
+        Chip8 {
+            memory: Self::_init_memory(filename),
+            gfx: [0; SCREEN_WIDTH * SCREEN_HEIGHT],
+            pc: PROGRAM_START,
+            stack: Vec::new(),
+            v: [0; 16],
+            i: 0,
+        }
+    }
+
+    fn _init_memory(filename: &str) -> [u8; MEMORY_SIZE] {
+        let rom_data = fs::read(filename).expect("Failed to open ROM file");
+
+        if rom_data.len() > (MEMORY_SIZE - PROGRAM_START) {
+            eprintln!("ROM file is too large to fit in memory");
+            std::process::exit(1);
+        }
+
+        let mut memory = [0u8; MEMORY_SIZE];
+
+        memory[PROGRAM_START..(PROGRAM_START + rom_data.len())].copy_from_slice(&rom_data);
+
+        memory[FONTSET_START..(FONTSET_START + 80)].copy_from_slice(&[
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+        ]);
+
+        memory
+    }
+
+    fn handle_input(&mut self) {}
+
+    fn update_timers(&mut self) {}
+
+    fn draw_sprite(&mut self, mut x: usize, mut y: usize, n: usize) {
+        let mut collision = 0;
+
+        x %= SCREEN_WIDTH;
+        y %= SCREEN_HEIGHT;
+
+        let max_rows = std::cmp::min(n, SCREEN_HEIGHT - y);
+        let max_cols = std::cmp::min(8, SCREEN_WIDTH - x);
+
+        for row in 0..max_rows {
+            let y_coord = (y + row) * SCREEN_WIDTH + x;
+            let sprite_byte = self.memory[self.i + row];
+
+            for col in 0..max_cols {
+                if (sprite_byte >> (7 - col)) & 1 == 1 {
+                    collision |= self.gfx[y_coord + col];
+                    self.gfx[y_coord + col] ^= 1;
+                }
+            }
+        }
+
+        self.v[0xF] = collision;
+    }
+
+    fn draw_to_screen(&self) {}
+
+    fn emulate_instruction(&mut self, how_many: usize) {
+        let mut opcode: u16;
+
+        for _ in 0..how_many {
+            opcode = ((self.memory[self.pc] as u16) << 8) | (self.memory[self.pc + 1] as u16);
+            self.pc += 2;
+
+            match opcode & 0xF000 {
+                0x0000 => match opcode & 0x00FF {
+                    // opcode 0x00E0, clear the display
+                    0x00E0 => self.gfx.fill(0),
+
+                    // opcode 0x00EE, return from subroutine
+                    0x00EE => self.pc = self.stack.pop().expect("Stack underflow"),
+
+                    _ => println!("Unknown opcode: {:#04X}", opcode),
+                },
+
+                // opcode 0x1NNN, jump to address NNN
+                0x1000 => self.pc = (opcode & 0x0FFF) as usize,
+
+                // opcode 0x6XNN, set register VX to NN
+                0x6000 => self.v[((opcode & 0x0F00) >> 8) as usize] = (opcode & 0x00FF) as u8,
+
+                // opcode 0xANNN, set index register I to NNN
+                0xA000 => self.i = (opcode & 0x0FFF) as usize,
+
+                // opcode 0xDXYN, draw sprite at coordinate (VX, VY) with height N
+                0xD000 => self.draw_sprite(
+                    self.v[((opcode & 0x0F00) >> 8) as usize] as usize,
+                    self.v[((opcode & 0x00F0) >> 4) as usize] as usize,
+                    (opcode & 0x000F) as usize,
+                ),
+
+                _ => println!("Unknown opcode: {:#04X}", opcode),
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <rom_file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let mut interpreter = Chip8::new(&args[1]);
+
+    interpreter.handle_input();
+    interpreter.update_timers();
+    interpreter.emulate_instruction(200);
+    interpreter.draw_to_screen();
 }
